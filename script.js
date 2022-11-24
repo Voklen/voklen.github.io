c343c2ead913a2d1483318d9ff5ab36239d0db06
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-11-24 11:10:36 +0000

    Adapt to variable framerates and refactor

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ function lerp(start, end, t) {
 function convert(integer) {
 	var str = parseInt(integer, 10).toString(16);
 	return str.length == 1 ? "0" + str : str;
-};
+}
+
 function to_hex(r, g, b) {
 	return "#" + convert(r) + convert(g) + convert(b);
 }
 
 function fadeColour(css_var, target, elem) {
-	let lerp_value = 0;
 	original = getComputedStyle(document.documentElement).getPropertyValue(css_var);
 	// Get rgb values and convert hex string to int with +
 	let orig_r = +("0x" + original[1] + original[2]);
@@ -52,21 +52,23 @@ function fadeColour(css_var, target, elem) {
 	let target_b = +("0x" + target[5] + target[6]);
 
 	// Set up animation
-	let id = setInterval(frame);
+	let id = setInterval(frame, 1 / FRAMERATE);
 	let anim_duration = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--theme-animation-duration").slice(0, -1));
 
-	function frame() { // Function frame() run every interval
-		if (lerp_value > 0.99999) { // If done (> because it's a float)
+	let lerp_value = 0;
+	function frame() {
+		if (lerp_value > 0.99999) {
 			clearInterval(id);
 			r.style.setProperty(css_var, target);
-		} else { // Iterate
-			r.style.setProperty(css_var, to_hex(  // Calculate, convert to hex, and set var to the colour
-				lerp(orig_r, target_r, lerp_value), // Lerp the red value
-				lerp(orig_g, target_g, lerp_value), // Lerp the green value
-				lerp(orig_b, target_b, lerp_value)  // Lerp the blue value
-			));
-			lerp_value += (1 / FRAMERATE) / anim_duration;
+			return;
 		}
+		let interpolated = to_hex(
+			lerp(orig_r, target_r, lerp_value), // Lerp the red value
+			lerp(orig_g, target_g, lerp_value), // Lerp the green value
+			lerp(orig_b, target_b, lerp_value)  // Lerp the blue value
+		)
+		r.style.setProperty(css_var, interpolated);
+		lerp_value += (1 / FRAMERATE) / anim_duration;
 	}
 }
 
