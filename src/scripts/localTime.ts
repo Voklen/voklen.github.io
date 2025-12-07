7624304c95a3ad6f179f36a8896c4bba302d3a75
Alex Gorichev <me@voklen.com>
2025-12-07 11:47:03 +0000

    Move style and script tags inside of the HTML tag
    I noticed there was malformed HTML through validator.w3.org

---
+++ src/scripts/localTime.ts
@@ -0,0 +1,16 @@
+function currentTimeInTZ(tzString: string) {
+	const currentTime = new Date();
+	return currentTime.toLocaleString("en-GB", {
+		timeZone: tzString,
+		timeStyle: "long",
+	});
+}
+
+function setFooterText() {
+	const footer = document.getElementsByTagName("footer")[0];
+	const currentTime = currentTimeInTZ("Europe/London");
+	footer.innerHTML = `Location: Scotland | Local time: ${currentTime}`;
+}
+
+setFooterText();
+setInterval(setFooterText, 1000);
