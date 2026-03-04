6ad0da3a0fe2f9f6bd962bbd60d8a1122178c10b
Alex Gorichev <alex@voklen.com>
2026-03-04 18:39:47 +0000

    Use `<Image>` for photos and add alt text

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.voklen.com",
+	image: {
+		responsiveStyles: true,
+		layout: "full-width",
+	},
 });
